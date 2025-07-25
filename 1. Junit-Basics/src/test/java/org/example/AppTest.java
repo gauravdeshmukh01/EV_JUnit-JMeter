89c18529b330185bb47ee21fd315d6988e229db1
gauravdeshmukh01 <gauravdeshmukh1010@gmail.com>
2025-07-25 20:12:10 +0530

    added day 2 learning

--- 1. Junit-Basics/src/test/java/org/example/AppTest.java
+++ 1. Junit-Basics/src/test/java/org/example/AppTest.java
@@ -1,12 +1,42 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class AppTest {
+
+    CalculationUtil cu;
+
+    @BeforeAll
+    static void beforeAllInit()       // static no need of instance
+    {
+        System.out.println("BeforeAll is executed");
+    }
+
+    @BeforeEach
+    void init()
+    {
+        System.out.println("---Before every test it will execute");
+        cu = new CalculationUtil();
+    }
+
+    @AfterEach
+    void cleanupCode()
+    {
+        System.out.println("Cleaning");
+    }
+
+    @AfterAll
+    static void afterAllTests()  // static
+    {
+        System.out.println("After all tests, All the test methods are executed");
+    }
+
+    // after commenting all above  go and add dependency surefire and in maven in test check all
+
 
     @Test// to make a method a test method,
     @DisplayName("SampleTest")
@@ -23,7 +53,7 @@ public class AppTest {
         CalculationUtil cu = new CalculationUtil();
         int actual = cu.add(10, 20);
         int expected = 30;
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -33,7 +63,7 @@ public class AppTest {
         CalculationUtil cu = new CalculationUtil();
         int actual = cu.mul(10, 20);
         int expected = 200;
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -60,7 +90,7 @@ public class AppTest {
         CalculationUtil cu = new CalculationUtil();
         int actual = cu.div(10, 2);
         int expected = 5;
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -87,9 +117,43 @@ public class AppTest {
         String s3 = new String("Hi"); // heap memory
         String s4 = new String("Hi"); // heap memory
         //Assertions.assertSame(s3, s4); // reference
-        Assertions.assertEquals(s3, s4); // content
+        assertEquals(s3, s4); // content
 
     }
+
+
+    @Test
+    public void demoTest() {
+        String str = "Info";
+        assertEquals("Infosys", str.concat("sys"));
+    }
+
+// assertAll - it will be true if all cases are true, it is similar AND condition
+
+    @Test
+    @DisplayName("AssertAll in multiple test")
+    void testMultiply() {
+        int actual = cu.mul(0, 5);
+        int actual2 = cu.mul(-2, -2);
+      //  assertEquals(0, actual);
+      //  assertEquals(4, actual2); // use 5 also
+        assertAll(
+                () -> assertEquals(0, cu.mul(0, 4)),
+                () -> assertEquals(4, cu.mul(-2, -2)), // if 6 used in place of 4 then fail
+                () -> assertEquals(-2, cu.mul(-2, 1))
+        );
+
+    }
+
+
+    @RepeatedTest(3)
+    @DisplayName("Repetition Test")
+    void myTest( RepetitionInfo repetitionInfo)
+    {
+        System.out.println(repetitionInfo.getCurrentRepetition());
+        assertEquals( "Hi",  "Hi");
+    }
+
 
 }
 
